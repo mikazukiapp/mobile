dd2daaec699603e2c92bd47cfa4714ca2a9634b6
NicoAiko <n.linnemann@nicoaiko.de>
2020-12-09 00:15:41 +0100

    Add information to list item widget Add next episode, progress, and time Reduce title to two lines with ellipsis on too long Put episode text directly below title Add isAiring icon Add text to see when next episode is airing Add progressbars for own progress and next episode progress

--- lib/mobile/widgets/AniList/Overview/ListItem.dart
+++ lib/mobile/widgets/AniList/Overview/ListItem.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:mikazuki/mobile/widgets/util/HexColor.dart';
+import 'package:mikazuki/shared/AniList/types/MediaStatus.dart';
 import 'package:mikazuki/shared/AniList/types/UserListEntry.dart';
 
 class AniListOverviewListItem extends StatefulWidget {
@@ -21,6 +24,10 @@ class _AniListOverviewListItemState extends State<AniListOverviewListItem> {
   double get score => widget.entry.score;
   double get progressPercentage => widget.entry.progressPercentage;
   String get progressText => widget.entry.progressText;
+  int get nextEpisode => widget.entry.media.nextAiringEpisode?.episode;
+  double get nextEpisodeProgress => widget.entry.nextEpisodeProgressPercentage;
+  String get airingAtDifference => widget.entry.media.nextAiringEpisode?.airingAtDifference;
+  bool get isAiring => widget.entry.media.status == AniListMediaStatus.Releasing;
 
   @override
   Widget build(BuildContext context) {
@@ -70,7 +77,7 @@ class _AniListOverviewListItemState extends State<AniListOverviewListItem> {
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(flex: 2, child: Text(title, maxLines: 3)),
+                      Expanded(flex: 2, child: Text(title, maxLines: 2, overflow: TextOverflow.ellipsis, semanticsLabel: title)),
                       Container(
                         padding: EdgeInsets.only(left: 4.0),
                         child: Row(
@@ -93,28 +100,70 @@ class _AniListOverviewListItemState extends State<AniListOverviewListItem> {
                       ),
                     ],
                   ),
-                  Spacer(),
                   Text(
-                    progressText,
+                    'Episode $progressText',
+                    maxLines: 1,
                     style: TextStyle(
-                      // color: Colors.grey[700],
+                      color: Colors.white70,
                       fontSize: 12.0,
                     ),
                   ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: LinearProgressIndicator(
-                      value: progressPercentage,
-                      valueColor: coverColor != null ? AlwaysStoppedAnimation<Color>(HexColor.fromHex(coverColor)) : null,
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    child: Row(
+                      children: [
+                        if (isAiring)
+                          Flexible(
+                            flex: 1,
+                            child: Icon(Icons.settings_input_antenna, size: 12.0, color: Colors.green,),
+                          ),
+                        if (nextEpisode != null)
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: isAiring ? 4.0 : 0),
+                            child: Text(
+                              'Episode $nextEpisode is airing in $airingAtDifference',
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 12.0,
+                              ),
+                            ),
+                          ),
+                      ],
                     ),
                   ),
-                  // Text(
-                  //   item.statusTitle,
-                  //   style: TextStyle(
-                  //     // color: Colors.grey[700],
-                  //     fontSize: 12.0,
-                  //   ),
-                  // ),
+                  Stack(
+                    alignment: Alignment.bottomCenter,
+                    overflow: Overflow.visible,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 6,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(2.0),
+                          child: LinearProgressIndicator(
+                            value: progressPercentage,
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              coverColor != null ? HexColor.fromHex(coverColor) : Colors.green[800],
+                            ),
+                            backgroundColor: coverColor != null ? HexColor.fromHex(coverColor).withOpacity(0.4) : Colors.green[800].withOpacity(0.4),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(2.0), bottomRight: Radius.circular(2.0)),
+                          child: LinearProgressIndicator(
+                            value: nextEpisodeProgress,
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              coverColor != null ? HexColor.fromHex(HexColor.calculateComplementaryColorFromHex(coverColor)) : Colors.orange[300]
+                            ),
+                            backgroundColor: Colors.transparent,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
