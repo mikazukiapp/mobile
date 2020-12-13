48d64070fa11249fc3443f3e92570c9e26ee0181
NicoAiko <n.linnemann@nicoaiko.de>
2020-12-13 21:37:34 +0100

    Remove old background image Remove old logo Use gradient

--- lib/mobile/pages/login.dart
+++ lib/mobile/pages/login.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mikazuki/mobile/app.dart';
+import 'package:mikazuki/mobile/constants.dart';
 import 'package:mikazuki/mobile/pages/search.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -84,58 +86,70 @@ class _LoginScreenWidgetState extends State<LoginScreenWidget> {
           children: [
             Container(
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/login_background.jpg'),
-                  fit: BoxFit.cover,
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Colors.green[800],
+                    Colors.blue[300],
+                  ],
+                  tileMode: TileMode.mirror,
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
                 ),
               ),
             ),
             Positioned.fill(
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-                child: Container(color: Colors.black.withOpacity(0.5)),
+                child: Container(color: Colors.black.withOpacity(0.4)),
               ),
             ),
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image.asset('assets/images/AppIcon.png'),
-                Text('Welcome to Mikazuki!'),
-                Text('To effectively use Mikazuki, please log in to AniList.'),
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: RaisedButton(
-                    onPressed: _launchAniListLoginSequence,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                    padding: EdgeInsets.all(0),
-                    child: Ink(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.green,
-                            Colors.lightBlue,
-                          ],
-                        ),
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: Container(
-                        constraints:
-                            BoxConstraints(minWidth: 88.0, minHeight: 36.0),
-                        alignment: Alignment.center,
-                        child: Text('Login', textAlign: TextAlign.center),
-                      ),
-                    ),
-                  ),
+                SvgPicture.asset(
+                  kMikazukiSvgLogo,
+                  width: MediaQuery.of(context).size.width - 56,
                 ),
-                TextButton(
-                  child: Text('Continue without login'),
-                  onPressed: () => Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                          builder: (context) => SearchScreenWidget())),
+                Column(
+                  children: [
+                    Text('Welcome to Mikazuki!'),
+                    Text('To effectively use Mikazuki, please log in to AniList.'),
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: RaisedButton(
+                        onPressed: _launchAniListLoginSequence,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        padding: EdgeInsets.all(0),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.green,
+                                Colors.lightBlue,
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                          child: Container(
+                            constraints:
+                                BoxConstraints(minWidth: 88.0, minHeight: 36.0),
+                            alignment: Alignment.center,
+                            child: Text('Login', textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      child: Text('Continue without login'),
+                      onPressed: () => Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                              builder: (context) => SearchScreenWidget())),
+                    ),
+                  ],
                 ),
               ],
             ),
