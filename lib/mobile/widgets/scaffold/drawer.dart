906e9038e15027825b829adac98fabd226f98ce4
NicoAiko <n.linnemann@nicoaiko.de>
2021-01-08 17:49:14 +0100

    Add container for multiple account setup temporarily change routes build logic

--- lib/mobile/widgets/scaffold/drawer.dart
+++ lib/mobile/widgets/scaffold/drawer.dart
@@ -1,8 +1,7 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hive/hive.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:mikazuki/shared/AniList/AniListRepository.dart';
 
 class MikazukiDrawer extends StatefulWidget {
   MikazukiDrawer({Key key}) : super(key: key);
@@ -16,141 +15,147 @@ class _MikazukiDrawerState extends State<MikazukiDrawer> {
   Box<dynamic> userDataBox;
   String userAvatarImageLink;
   String userBannerImage;
+  String username;
 
-  List<Widget> routes = <Widget>[
-    ListTile(
-      title: Text('Lists'),
-      leading: Icon(Icons.list),
-    ),
-    ListTile(
-      title: Text('Search'),
-      leading: Icon(Icons.search),
-    ),
-    // TODO: Reimplement when working on settings page
-    // Divider(),
-    // ListTile(
-    //   title: Text('Settings'),
-    //   leading: Icon(Icons.settings),
-    // ),
-  ];
+  // TODO: Use Navigator 2.0 for navigation
+  List<Widget> _getRoutes(BuildContext context) {
+    bool isLoggedIn = AniListRepository.getInstance().isLoggedIn;
+    List<Widget> routes = [];
 
-  Map<String, int> routeIndicies = {
-    '/overview': 1,
-    '/search': 2,
-    // '/settings': 4,
-  };
+    routes.add(
+      UserAccountsDrawerHeader(
+        accountName: Text(username),
+        accountEmail: Text(
+          "Next update in: To be implemented",
+          style: TextStyle(fontSize: 12.0),
+        ),
+        currentAccountPicture: _accountPicture(context),
+      ),
+    );
 
-  List<String> routeNames = [
-    null,
-    '/overview',
-    '/search',
-    // null,
-    // '/settings',
-  ];
-
-  Widget _getRoute(BuildContext context, int index) {
-    bool isSelected = _selectedIndex == index;
-
-    dynamic widget = routes[index];
-    String route = routeNames[index];
-
-    if (widget is ListTile) {
-      return ListTile(
-          title: widget.title,
-          leading: widget.leading,
-          selected: isSelected,
+    if (isLoggedIn) {
+      routes.add(
+        ListTile(
+          title: Text('Lists'),
+          leading: Icon(Icons.list),
+          selected: _selectedIndex == 1,
           onTap: () {
-            Navigator.pushNamed(context, route);
-          });
+            Navigator.pushNamed(context, '/overview');
+          },
+        ),
+      );
+    } else {
+      routes.add(
+        ListTile(
+          title: Text('Login'),
+          leading: Icon(Icons.login),
+          selected: _selectedIndex == 1,
+          onTap: () {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              '/login',
+              (route) => false,
+            );
+          },
+        ),
+      );
     }
 
-    return widget;
+    routes.add(
+      ListTile(
+        title: Text('Search'),
+        leading: Icon(Icons.search),
+        selected: _selectedIndex == 2,
+        onTap: () {
+          if (Navigator.canPop(context)) {
+            Navigator.pop(context);
+          }
+
+          Navigator.pushNamed(context, '/search');
+        },
+      ),
+    );
+
+    return routes;
+  }
+
+  Widget _accountPicture(BuildContext context) {
+    if (userAvatarImageLink != null) {
+      return CircleAvatar(
+        backgroundImage: NetworkImage(
+          userAvatarImageLink,
+        ),
+        radius: 4.0,
+      );
+    }
+
+    return null;
   }
 
   @override
   void initState() {
     super.initState();
     _selectedIndex = 0;
-    userDataBox = Hive.box('anilist_userdata');
-    userAvatarImageLink = userDataBox.get('avatars')?.getSmallest();
-    userBannerImage = userDataBox.get('bannerImage');
-
-    routes.insert(
-      0,
-      DrawerHeader(
-        child: Container(
-          alignment: Alignment.centerLeft,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Flexible(
-                flex: 1,
-                child: ClipRRect(
-                  child: CachedNetworkImage(
-                    imageUrl: userAvatarImageLink,
-                    progressIndicatorBuilder:
-                        (context, url, downloadProgress) =>
-                            CircularProgressIndicator(
-                                value: downloadProgress.progress),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
-                  ),
-                  borderRadius: BorderRadius.circular(4.0),
-                ),
-              ),
-              Flexible(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Text('NicoAiko'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-
-    Future.delayed(Duration.zero, () {
-      String routeName = ModalRoute.of(context).settings.name;
-      setState(() {
-        _selectedIndex = routeIndicies[routeName] ?? 0;
-      });
-    });
+    if (Hive.isBoxOpen('anilist_userdata')) {
+      userDataBox = Hive.box('anilist_userdata');
+      userAvatarImageLink = userDataBox.get('avatars')?.getSmallest();
+      userBannerImage = userDataBox.get('bannerImage');
+      username = userDataBox.get('username');
+    } else {
+      userAvatarImageLink = null;
+      userBannerImage = null;
+      username = "Unauthorized";
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Stack(
+      child: Row(
         children: [
-          if (userBannerImage != null)
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(userBannerImage),
+          Flexible(
+            flex: 1,
+            fit: FlexFit.tight,
+            child: SafeArea(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    right: BorderSide(
+                      color:
+                          Theme.of(context).backgroundColor.withOpacity(0.25),
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      width: 48,
+                      height: 48,
+                      margin: EdgeInsets.all(8.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white),
+                      ),
+                      child: SvgPicture.asset(
+                          "assets/images/appLogos/AniListLogo.svg"),
+                    ),
+                  ],
                 ),
               ),
             ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.75),
-            ),
           ),
-          BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
-            child: Container(
-              color: Colors.black.withOpacity(0),
+          Expanded(
+            flex: 4,
+            child: ListView.builder(
+              itemCount: _getRoutes(context).length,
+              itemBuilder: (context, index) {
+                return _getRoutes(context)[index];
+              },
+              // Seems to be important
+              padding: EdgeInsets.zero,
             ),
-          ),
-          ListView.builder(
-            itemCount: routes.length,
-            itemBuilder: (context, index) {
-              return _getRoute(context, index);
-            },
-            // Seems to be important
-            padding: EdgeInsets.zero,
           ),
         ],
       ),
