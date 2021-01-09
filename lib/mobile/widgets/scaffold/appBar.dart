0992ef4777ac760976e6459021cb52fb4c413b29
NicoAiko <n.linnemann@nicoaiko.de>
2021-01-09 18:28:29 +0100

    Add animated appbar

--- lib/mobile/widgets/scaffold/appBar.dart
+++ lib/mobile/widgets/scaffold/appBar.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mikazuki/mobile/constants.dart';
-import 'package:mikazuki/mobile/pages/login.dart';
-import 'package:mikazuki/mobile/widgets/util/NoAnimationMaterialPageRoute.dart';
-import 'package:mikazuki/shared/AniList/AniListRepository.dart';
 
 class MikazukiAppBar extends StatefulWidget implements PreferredSizeWidget {
   final String title;
@@ -17,35 +14,154 @@ class MikazukiAppBar extends StatefulWidget implements PreferredSizeWidget {
   Size get preferredSize => new Size.fromHeight(kToolbarHeight);
 }
 
-class MikazukiAppBarState extends State<MikazukiAppBar> {
-  @override
-  Widget build(BuildContext context) {
+class MikazukiAppBarState extends State<MikazukiAppBar>
+    with SingleTickerProviderStateMixin {
+  TextEditingController _textController;
+  AnimationController _animationController;
+  Animation<Offset> _offsetAnimation;
+  bool _isQuickSearchActive;
+
+  Widget _buildQuickSearchField(BuildContext context) {
     return AppBar(
       elevation: 0,
       leading: IconButton(
-          icon: SvgPicture.asset(kMikazukiSvgLogo),
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          _animationController.forward().then((_) {
+            setState(() {
+              _isQuickSearchActive = !_isQuickSearchActive;
+            });
+
+            _animationController.reverse();
+          });
+        },
+      ),
+      title: TextField(
+        autocorrect: true,
+        controller: _textController,
+        onSubmitted: _onQuickSearchSubmit,
+        keyboardType: TextInputType.text,
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+          hintText: 'Title, Genre, Synonyms...',
+          labelText: 'Search query...',
+          hintStyle: Theme.of(context).primaryTextTheme.subtitle1,
+          labelStyle: Theme.of(context).primaryTextTheme.subtitle1,
+          border: InputBorder.none,
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.clear),
           onPressed: () {
-            Scaffold.of(context).openDrawer();
-          }),
+            _textController.clear();
+          },
+        ),
+      ],
+    );
+  }
+
+  void _onQuickSearchSubmit(String query) {
+    if (query.trim().length == 0) {
+      return;
+    }
+
+    print(query);
+
+    Navigator.pushNamed(context, '/search');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textController = TextEditingController();
+    _isQuickSearchActive = false;
+
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(1.5, 0),
+    ).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    _animationController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget child = _isQuickSearchActive
+        ? _buildQuickSearchField(context)
+        : _buildAppBar(context);
+
+    return SlideTransition(
+      position: _offsetAnimation,
+      child: child,
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset(kMikazukiSvgLogo),
+        onPressed: () {
+          Scaffold.of(context).openDrawer();
+        },
+      ),
       title: widget.title != null ? Text(widget.title) : null,
       actions: <Widget>[
         IconButton(
-            icon: Icon(AniListRepository.getInstance().isLoggedIn
-                ? Icons.logout
-                : Icons.login),
-            onPressed: () {
-              if (AniListRepository.getInstance().isLoggedIn) {
-                print('Logging out...');
-                AniListRepository.getInstance().logout();
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    NoAnimationMaterialPageRoute(
-                        builder: (BuildContext context) => LoginScreenWidget()),
-                    (route) => false);
-              } else {
-                Navigator.pushNamed(context, '/login');
-              }
-            }),
+          icon: Icon(Icons.search),
+          tooltip: 'Search',
+          onPressed: () {
+            _animationController.forward().then((_) {
+              setState(() {
+                _isQuickSearchActive = !_isQuickSearchActive;
+              });
+
+              _animationController.reverse();
+            });
+          },
+        ),
+        PopupMenuButton(
+          tooltip: 'Filtering and Sorting',
+          padding: EdgeInsets.zero,
+          icon: Icon(Icons.sort),
+          offset: Offset(0, 56.0),
+          itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+            const PopupMenuItem(
+              enabled: false,
+              child: Text('Sorting (to be implemented...)'),
+            ),
+            const PopupMenuItem(
+              enabled: false,
+              child: ListTile(
+                dense: true,
+                leading: Icon(Icons.title),
+                title: Text('Sort by title'),
+              ),
+            ),
+            const PopupMenuItem(
+              enabled: false,
+              child: ListTile(
+                dense: true,
+                leading: Icon(Icons.tv),
+                title: Text('Sort by episode amount'),
+              ),
+            ),
+          ],
+        ),
         //   IconButton(
         //       icon: appTheme.currentTheme() == ThemeMode.light
         //           ? Icon(Icons.lightbulb, color: Colors.yellow)
